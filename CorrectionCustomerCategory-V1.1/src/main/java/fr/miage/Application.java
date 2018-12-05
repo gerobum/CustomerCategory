81f7e92f45082a81620e14cf9b4da839ceddba12
Yvan Maillot <yvan.maillot@uha.fr>
2018-12-05 23:53:31 +0100

    V1.1 - Remplissage de quelques clients et quelques catégories de démo dans la BDD au démarrage de l'application.      - Et utilisation d'un "logger" pour afficher des messages.

---
+++ CorrectionCustomerCategory-V1.1/src/main/java/fr/miage/Application.java
@@ -0,0 +1,92 @@
+// #### V0.0 Application "Customers" qui permet de lister, ajouter et modifier
+// #### V0.0 des clients dans une base de données.
+
+// #### V0.1 Internationalisation
+
+// #### V0.2 Les effets de l'internalisation faite précédemment ne sont visibles
+// #### V0.2 que si l'on change réellement la localisation par exemple en modifiant 
+// #### V0.2 les préférences de firefox.
+// #### V0.2 Dans cette version, un menu est rajouté sur toutes les pages pour
+// #### V0.2 permettre le changement de localisation à l'aide de simples boutons.
+
+// #### V0.2 On profitera pour ajouter des titres, en-tête et pied de page
+// #### V0.2 sur toutes les pages.
+
+// #### V0.3 Utilisation de Bootstrap et js pour avoir une jolie interface.
+// #### V0.3 - Les fichiers js et Boostrap sont dans src/main/resources/static
+// #### V0.3 - Le dossier src/main/resources/static est déclaré ressources dans SpringWebConfig 
+// #### V0.3 - Les templates .html sont agrémentés de décorations bootstap
+
+// #### V1.0 Rajout d'une entité Category à gérer comme c'est fait pour les clients.
+// #### V1.0 C'est-à-dire la possibilité de créer, de modifier et de supprimer
+// #### V1.0 des catégories. La seule différente est que le nom d'une catégorie
+// #### V1.0 n'a qu'une et une seule lettre.
+// #### V1.0 Ajout d'un menu visible sur toutes les pages qui permet de choisir
+// #### V1.0 entre la page qui affiche les clients et celle qui affiche les 
+// #### V1.0 catégories, à l'instar de celui qui permet de basculer entre 
+// #### V1.0 "anglais" et "français". 
+
+// #### V1.1 Remplissage de quelques clients et quelques catégories de démo
+// #### V1.1 dans la BDD au démarrage de l'application. Et utilisation d'un  
+// #### V1.1 "logger" pour afficher des messages.
+package fr.miage;
+
+import fr.miage.config.SpringWebConfig;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// #### V0.0 @SpringBootApplication est un raccourci pour 
+// #### V0.0 @EnableAutoConfiguration
+// #### V0.0 @ComponentScan
+// #### V0.0 @Configuration
+// #### V0.0 https://docs.spring.io/spring-boot/docs/current/reference/html/using-boot-using-springbootapplication-annotation.html
+@SpringBootApplication(scanBasePackages = "fr.miage")
+// #### V0.0 "fr.miage" précise à partir d'où commence le scan des beans.
+public class Application extends SpringBootServletInitializer implements WebMvcConfigurer {
+    public static final String MAPPING_WEB = "/";
+    public static final String CHARACTER_ENCODING = "UTF-8";
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean webServlet() {
+        DispatcherServlet dispatcherServlet = new DispatcherServlet();
+
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.register(SpringWebConfig.class);
+        dispatcherServlet.setApplicationContext(applicationContext);
+
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(dispatcherServlet, MAPPING_WEB);
+        servletRegistrationBean.setName("webServlet");
+
+        return servletRegistrationBean;
+    }
+
+    @Override
+    public void onStartup(ServletContext container) {
+        // Create the 'root' Spring application context
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        //rootContext.register(SpringCoreConfig.class);
+
+        // Manage the lifecycle of the root application context
+        container.addListener(new ContextLoaderListener(rootContext));
+
+        //
+        FilterRegistration characterEncodingFilter = container.addFilter("CharacterEncodingFilter", CharacterEncodingFilter.class);
+        characterEncodingFilter.setInitParameter("encoding", CHARACTER_ENCODING);
+        characterEncodingFilter.setInitParameter("forceEncoding", "true");
+        characterEncodingFilter.addMappingForUrlPatterns(null, false, MAPPING_WEB);
+    }
+}
