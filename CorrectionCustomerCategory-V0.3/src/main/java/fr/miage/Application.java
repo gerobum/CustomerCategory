b6ad97abd3fcfd902e4594a144a144fbd2b93a73
Yvan Maillot <yvan.maillot@uha.fr>
2018-12-06 00:13:05 +0100

    V0.2 - V0.3 - V01 - Suppression de choses inutiles dans "public class Application"

--- CorrectionCustomerCategory-V0.3/src/main/java/fr/miage/Application.java
+++ CorrectionCustomerCategory-V0.3/src/main/java/fr/miage/Application.java
@@ -46,34 +46,4 @@ public class Application extends SpringBootServletInitializer implements WebMvcC
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
-
-    @Bean
-    public ServletRegistrationBean webServlet() {
-        DispatcherServlet dispatcherServlet = new DispatcherServlet();
-
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
-        applicationContext.register(SpringWebConfig.class);
-        dispatcherServlet.setApplicationContext(applicationContext);
-
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(dispatcherServlet, MAPPING_WEB);
-        servletRegistrationBean.setName("webServlet");
-
-        return servletRegistrationBean;
-    }
-
-    @Override
-    public void onStartup(ServletContext container) {
-        // Create the 'root' Spring application context
-        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        //rootContext.register(SpringCoreConfig.class);
-
-        // Manage the lifecycle of the root application context
-        container.addListener(new ContextLoaderListener(rootContext));
-
-        //
-        FilterRegistration characterEncodingFilter = container.addFilter("CharacterEncodingFilter", CharacterEncodingFilter.class);
-        characterEncodingFilter.setInitParameter("encoding", CHARACTER_ENCODING);
-        characterEncodingFilter.setInitParameter("forceEncoding", "true");
-        characterEncodingFilter.addMappingForUrlPatterns(null, false, MAPPING_WEB);
-    }
 }
